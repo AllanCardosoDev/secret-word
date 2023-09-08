d7b8d1304429e09a47907a0d0b1afdfb9e2a695b
Allan Cardoso Araujo <allancardoso.dev@gmail.com>
2023-09-07 22:31:38 -0300

    first commit

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Volante"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Limão"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos",],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "Python"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+};
